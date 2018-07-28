823ce38c91eb5712eeb5953b516fd83038428a1c
Tom <tjpalmer@tjpalmer.com>
2018-07-28 11:29:47 -0700

    Magically broken attempt at using vars

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
-bion -o bin\out_plort.c plort ^
+bion -notypeinfo -o bin\out_plort.c plort ^
     && cl /Fe:bin\plort /Fo:bin\plort bin\out_plort.c ^
     /I import\angle\include /I import\sdl2\include /link ^
     /LIBPATH:import\sdl2\lib\x64 /LIBPATH:import\angle\lib\x64 ^
